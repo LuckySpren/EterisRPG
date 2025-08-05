1b832c509df47f537badd2271091788792fa6d0c
LuckySpren <lucasniper61@gmail.com>
2025-08-04 23:59:09 -0300

    Add files via upload

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    {
+        ignores: [
+            'build/',
+            'scripts/',
+            'src/declarations/',
+            'eslint.config.mjs',
+            'rollup.config.js',
+            'commitlint.config.js',
+            'lint-staged-config.mjs',
+            'scripts/',
+        ],
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        rules: {
+            '@typescript-eslint/no-namespace': 'off',
+            '@typescript-eslint/no-base-to-string': 'off',
+            '@typescript-eslint/no-misused-promises': 'off',
+            '@typescript-eslint/no-unsafe-argument': 'off',
+            '@typescript-eslint/no-unsafe-return': 'off',
+            '@typescript-eslint/no-empty-function': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            '@typescript-eslint/triple-slash-reference': 'off',
+            'no-unexpected-multiline': 'off',
+        },
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+                allowDefaultProject: ['*.js'],
+            },
+        },
+    },
+);
